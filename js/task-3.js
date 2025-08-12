cb2cb99fffb316d26503b4453f492845728c03f0
helen-akateva <akareva17@gmail.com>
2025-08-12 10:49:57 +0300

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,9 @@
 function checkForSpam(message) {
-  const lowerCaseMessage = message.toLowerCase();
+  const normalizedMessage = message.toLowerCase();
 
-  return lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale');
+  return (
+    normalizedMessage.includes('spam') || normalizedMessage.includes('sale')
+  );
 }
 console.log(checkForSpam('Latest technology news')); // false
 console.log(checkForSpam('JavaScript weekly newsletter')); // false
